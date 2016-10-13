8442dcf63bd5847f4f9670439c8700203f02803e
Darren King <darrenbrettking@hotmail.com>
2016-10-13 08:50:53 -0700

    Added routing

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,11 +1,11 @@
 import { Component, OnInit, EventEmitter, Output } from '@angular/core';
+
 import { Recipe } from '../recipe';
 import { RecipeService } from '../recipe.service';
 
 @Component({
   selector: 'app-recipe-list',
-  templateUrl: './recipe-list.component.html',
-  styleUrls: ['./recipe-list.component.css']
+  templateUrl: 'recipe-list.component.html'
 })
 export class RecipeListComponent implements OnInit {
   recipes: Recipe[] = [];
--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -4,18 +4,22 @@ import { Ingredient } from '../shared/ingredient';
 
 @Injectable()
 export class RecipeService {
-private recipes: Recipe[] = [
-    new Recipe('Diavora', 'When you need a delish snack!', 'https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcQZIYR2xtyZ0oLbVvUaLRshEBLcjnDUb9aiDWRIPttFKAnCLLBs', []),
-    new Recipe('Tisasvoa', 'A great Russian delicacy!', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ0whL95_P_WBUuZ3sDUWV398PSWUT1zKZ5roqufZAXgadXe-bDZQ', []),
-    new Recipe('Croan Sliders', 'A northern Irish specialty!', 'https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcSeCj1UFAwjU-lY1_CT9Vhqg3AaG65rkRyA_HFP4F7zVgc_7d4SyQ', []),
-    new Recipe('Vinti Soup', 'Something spicy!', 'https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcRriEWt0_JnqqI38GSVVA2tEAgoh_4ajUbbESrsn8IOZ743KW9o', [])
+  private recipes: Recipe[] = [
+    new Recipe('Diavora', 'When you need a delish snack!', 'https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcQZIYR2xtyZ0oLbVvUaLRshEBLcjnDUb9aiDWRIPttFKAnCLLBs', [
+      new Ingredient('French Fries', 2),
+      new Ingredient('Pork Meat', 1)
+    ]),
+    new Recipe('Tisavola', 'A great Russian delicacy!', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ0whL95_P_WBUuZ3sDUWV398PSWUT1zKZ5roqufZAXgadXe-bDZQ', [
+      new Ingredient('Backaman', 3),
+      new Ingredient('Peppered Beef', 2)
+    ])
   ];
 
 
   constructor() { }
 
   getRecipes() {
-    return this.recipes;
+    return this.getRecipes;
   }
 
 }
